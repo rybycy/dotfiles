4af7bb424b63a4fca91ee863e991852e51a04b4e
Michał Robaszyński <michal.robaszynski@gmail.com>
2018-11-13 20:22:43 +0100

    Update init.lua

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,1 +1,20 @@
-/Users/robaszynskim/.dotfiles/dotfiles/hammerspoon/init.lua
+local hyper = {"ctrl", "cmd", "shift", "alt"}
+
+launch = function(appname)
+  hs.application.launchOrFocus(appname)
+end
+
+-- Single keybinding for app launch
+singleapps = {
+  {'q', 'Microsoft Outlook'},
+  {'d', 'iTerm'},
+  {'space', 'Sublime Text'},
+  {'e', 'Google Chrome'},
+  {'c', 'IntelliJ IDEA CE'},
+  {'s', 'Skype for Business'}
+}
+
+for i, app in ipairs(singleapps) do
+  hs.hotkey.bind(hyper, app[1], function() launch(app[2]); end)  
+end
+
